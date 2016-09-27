e345b380a084acf8d1ba5e0e8139402ae324c289
JasonHsu <xuzhepku@gmail.com>
2016-09-27 17:28:16 +0800

    debounceTime的优化

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {Data} from "../../providers/data/data";
+import {Control} from "@angular/common";
+import 'rxjs/add/operator/debounceTime';// 必须要加, 否则debounceTime不生效
 
 @Component({
     templateUrl : 'build/pages/home/home.html',
@@ -9,15 +11,27 @@ import {Data} from "../../providers/data/data";
 export class HomePage {
     searchTerm: string = '';
     items: any;
+    searchControl: Control;
+    searching: any = false;
 
     constructor(public navCtrl: NavController, private dataService: Data) {
+        this.searchControl = new Control();
     }
 
     ionViewLoaded() {
         this.setFilteredItems();
+        this.searchControl.valueChanges.debounceTime(700).subscribe(search => {
+            this.searching = false;
+            this.setFilteredItems();
+        });
+    }
+
+    onSearchInput() {
+        this.searching = true;
     }
 
     setFilteredItems() {
         this.items = this.dataService.filterItems(this.searchTerm);
+
     }
 }
